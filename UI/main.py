6bee1d2623b114132c6d02faba6d35b320d5e5a8
dandeeAndy <schahn131@koreatech.ac.kr>
2024-02-11 23:36:00 +0900

    Delta_System 추가 및 python 파일 추가

---
+++ UI/main.py
@@ -0,0 +1,93 @@
+
+
+import sys
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(1920, 1080)
+        font = QtGui.QFont()
+        font.setPointSize(30)
+        MainWindow.setFont(font)
+        MainWindow.setStyleSheet("background-color:black;")
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.horizontalLayoutWidget = QtWidgets.QWidget(self.centralwidget)
+        self.horizontalLayoutWidget.setGeometry(QtCore.QRect(1040, 620, 803, 324))
+        self.horizontalLayoutWidget.setObjectName("horizontalLayoutWidget")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.horizontalLayoutWidget)
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.horizontalLayout.setObjectName("horizontalLayout")
+        self.label = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label.setEnabled(True)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
+        self.label.setSizePolicy(sizePolicy)
+        self.label.setFrameShape(QtWidgets.QFrame.NoFrame)
+        self.label.setFrameShadow(QtWidgets.QFrame.Plain)
+        self.label.setLineWidth(0)
+        self.label.setText("")
+        self.label.setTextFormat(QtCore.Qt.AutoText)
+        self.label.setPixmap(QtGui.QPixmap("123층_1누끼.png"))
+        self.label.setScaledContents(False)
+        self.label.setAlignment(QtCore.Qt.AlignCenter)
+        self.label.setIndent(-1)
+        self.label.setObjectName("label")
+        self.horizontalLayout.addWidget(self.label)
+        self.label_2 = QtWidgets.QLabel(self.horizontalLayoutWidget)
+        self.label_2.setText("")
+        self.label_2.setPixmap(QtGui.QPixmap("ABCD위치_A누끼.png"))
+        self.label_2.setScaledContents(False)
+        self.label_2.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_2.setIndent(0)
+        self.label_2.setObjectName("label_2")
+        self.horizontalLayout.addWidget(self.label_2)
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(QtCore.QRect(170, 30, 1071, 61))
+        self.label_3.setObjectName("label_3")
+        self.horizontalScrollBar = QtWidgets.QScrollBar(self.centralwidget)
+        self.horizontalScrollBar.setGeometry(QtCore.QRect(100, 686, 731, 20))
+        self.horizontalScrollBar.setOrientation(QtCore.Qt.Horizontal)
+        self.horizontalScrollBar.setObjectName("horizontalScrollBar")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 1920, 26))
+        self.menubar.setObjectName("menubar")
+        self.menuSCADA_System = QtWidgets.QMenu(self.menubar)
+        self.menuSCADA_System.setObjectName("menuSCADA_System")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.menubar.addAction(self.menuSCADA_System.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+        self.initUI()
+
+    def initUI(self):
+        self.statusBar().showMessage('Ready')
+
+        self.setWindowTitle('Statusbar') #상태바
+        self.setGeometry(300, 300, 300, 200)
+        self.show()
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.label_3.setText(_translate("MainWindow", "TextLabel"))
+        self.menuSCADA_System.setTitle(_translate("MainWindow", "SCADA System"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
