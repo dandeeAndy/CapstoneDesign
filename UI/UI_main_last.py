0969b70b2ec04ce34a7bcae2e4e060d3ec304ed3
dandeeAndy <schahn131@koreatech.ac.kr>
2024-04-09 20:26:18 +0900

    수정

--- UI/UI_main_last.py
+++ UI/UI_main_last.py
@@ -1,17 +1,13 @@
-import sys, os
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-import UI_set
-
-
+import sys
+from PyQt5.QtWidgets import QApplication
+import threading
+import UI_module  # UI_module 모듈을 임포트해
 
 from queue import Queue
 import socket
 import threading
 import time
 
-# -----------------------------------------------------------------------
 print(socket.gethostbyname(socket.gethostname()))
 
 Vision_Motor_host ='192.168.95.231'
@@ -21,9 +17,9 @@ port = 1111
 
 lock = threading.Lock()
 client_soc = None  # 전역 변수로 선언하여 모든 함수에서 접근 가능하게 함
+last_sent_option = None
 selected_option = None
 
-# -----------------------------------------------------------------------
 def client_func():
     global client_soc
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -49,51 +45,50 @@ def client_func():
         classifi = parts[0]
         
         if classifi[0] == 'L' or 'Y' or 'A':
-            UI_set.MainWindow.first_details_list_widget.addItem(qr_data_receive)
+            UI_module.MainWindow.first_details_list_widget.addItem(qr_data_receive)
         
         elif classifi[0] == 'F' or 'N' or 'B':
-            UI_set.MainWindow.second_details_list_widget.addItem(qr_data_receive)
+            UI_module.MainWindow.second_details_list_widget.addItem(qr_data_receive)
         
 # -----------------------------------------------------------------------
+last_sent_option = None
+
 def server_func():
-    global client_soc, selected_option
+    global client_soc, selected_option, last_sent_option
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((UI_host, port))  
+    server_socket.bind((UI_host, port))
     server_socket.listen()
     print('UI server waiting for connection....')
     
     client_soc, addr = server_socket.accept()
     print('UI server connected')
-    
+
     while True:
-        if selected_option is not None:
+        if selected_option is not None and selected_option != last_sent_option:
             try:
-                client_soc.sendall(selected_option.encode('utf-8'))
-                selected_option = None  # 메시지 전송 후 변수 초기화
+                client_soc.sendall((selected_option + '\n').encode('utf-8'))
+                last_sent_option = selected_option
+                selected_option = None  # 선택된 옵션이 전송되었으므로 초기화
             except socket.error as e:
                 print("Error sending data:", e)
                 break
-
-# -----------------------------------------------------------------------
-def UI_func():
+            
+def main():
     app = QApplication(sys.argv)
-    font = QFont("NanumSquare", 9)
-    app.setFont(font)
-    mainWin = UI_set.MainWindow()
+    mainWin = UI_module.MainWindow()
+
+    # for button in mainWin.option_buttons:
+    #     button.optionSelected.connect(send_option_to_server)  # 신호에 함수 연결
+
     mainWin.showMaximized()
-    mainWin.show()
     sys.exit(app.exec_())
     
-# -----------------------------------------------------------------------
+    
 if __name__ == '__main__':
+    main_thread = threading.Thread(target=main)
     server_thread = threading.Thread(target=server_func)
-    client_thread = threading.Thread(target=client_func)
-    UI_thread = threading.Thread(target=UI_func)
-
+    # client_thread = threading.Thread(target=client_func)  # 필요한 경우
+    
+    main_thread.start()
     server_thread.start()
-    client_thread.start()
-    UI_thread.start()
-
-    server_thread.join()
-    client_thread.join()
-    UI_thread.join()
+    # client_thread.start()  # 필요한 경우
