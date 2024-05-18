3002bd8d0c7b3a2a654bf4c881bfa0f4e442b4fe
dandeeAndy <schahn131@koreatech.ac.kr>
2024-05-18 23:19:40 +0900

    수정 및 거의 완성

--- UI/UI_set_impossible.py
+++ UI/UI_set_impossible.py
@@ -4,6 +4,8 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 
 font_title = QFont("NanumSquare", 12)
+font_bold = QFont("NanumSquare", 12)
+font_bold.setBold(True)
 border_style_1 = "border-top: 2px solid black; border-left: 2px solid black;"
 border_style_2 = "border-top: 2px solid black; border-left: 2px solid black; border-right: 2px solid black;"
 border_style_3 = "border-top: 2px solid black; border-left: 2px solid black; border-bottom: 2px solid black;"
@@ -21,7 +23,7 @@ class MainWindow(QMainWindow):
         central_widget.setLayout(self.grid_layout)
         self.setCentralWidget(central_widget)
 
-        self.grid_layout.setSpacing(5)
+        self.grid_layout.setSpacing(0)
         self.grid_layout.setContentsMargins(50, 0, 50, 50)
         
         # 각 열에 대한 비율 설정
@@ -40,41 +42,42 @@ class MainWindow(QMainWindow):
         self.grid_layout.setColumnStretch(3, 4)
         self.grid_layout.setColumnStretch(4, 36)
         
-        self.grid_layout.setColumnStretch(5, 1)
+        self.grid_layout.setColumnStretch(5, 5)
         
         # [분류] 현재위치/패키지번호/이메일/목적지/차주전화번호
-        self.grid_layout.setColumnStretch(6, 12)
-        self.grid_layout.setColumnStretch(7, 17)
-        self.grid_layout.setColumnStretch(8, 35)
-        self.grid_layout.setColumnStretch(9, 11)
-        self.grid_layout.setColumnStretch(10, 30)
+        self.grid_layout.setColumnStretch(6, 12) # [분류] 현재위치
+        self.grid_layout.setColumnStretch(7, 17) # [분류] 패키지번호
+        self.grid_layout.setColumnStretch(8, 35) # [분류] 이메일
+        self.grid_layout.setColumnStretch(9, 11) # [분류] 목적지
         
-        self.grid_layout.setColumnStretch(11, 1)
+        self.grid_layout.setColumnStretch(10, 30) # Option1 # [분류] 차주전화번호
+        self.grid_layout.setColumnStretch(11, 1) 
+        self.grid_layout.setColumnStretch(12, 12) # [분류] 현재위치
+        self.grid_layout.setColumnStretch(13, 2) # [분류] 패키지번호~
         
-        self.grid_layout.setColumnStretch(12, 12) # Option1
-        self.grid_layout.setColumnStretch(13, 17)
-        self.grid_layout.setColumnStretch(14, 4)
+        self.grid_layout.setColumnStretch(14, 15) # Option2 # [분류] ~패키지번호
+        self.grid_layout.setColumnStretch(15, 4)  # [분류] 이메일~
+        self.grid_layout.setColumnStretch(16, 26) # [분류] ~
         
-        self.grid_layout.setColumnStretch(15, 31) # Option2 # 
-        self.grid_layout.setColumnStretch(16, 2)
-        
-        self.grid_layout.setColumnStretch(17, 9) # Option3
-        self.grid_layout.setColumnStretch(18, 30)
+        self.grid_layout.setColumnStretch(17, 5) # Option3 # [분류] ~이메일
+        self.grid_layout.setColumnStretch(18, 2) # [분류] 목적지~
+        self.grid_layout.setColumnStretch(19, 9) # [분류] ~목적지
+        self.grid_layout.setColumnStretch(20, 30) # [분류] 차주전화번호
         
         label_1 = QLabel()
         self.grid_layout.addWidget(label_1, 0, 0, 1, 5)
         label_2 = QLabel()
         self.grid_layout.addWidget(label_2, 0, 5, 1, 14)
         self.label_3 = QLabel()
-        self.grid_layout.addWidget(self.label_3, 1, 0, 2, 11)
+        self.grid_layout.addWidget(self.label_3, 1, 0, 2, 9)
         label_4 = QLabel()
-        self.grid_layout.addWidget(label_4, 1, 12, 1, 3)
+        self.grid_layout.addWidget(label_4, 1, 10, 1, 4)
         label_5 = QLabel()
-        self.grid_layout.addWidget(label_5, 1, 15, 1, 2)
+        self.grid_layout.addWidget(label_5, 1, 14, 1, 3)
         label_6 = QLabel()
-        self.grid_layout.addWidget(label_6, 1, 17, 1, 2)
+        self.grid_layout.addWidget(label_6, 1, 17, 1, 4)
         label_7 = QLabel()
-        self.grid_layout.addWidget(label_7, 2, 12, 1, 7)
+        self.grid_layout.addWidget(label_7, 2, 10, 1, 11)
         label_8 = QLabel()
         self.grid_layout.addWidget(label_8, 3, 0, 1, 5)
         self.label_9 = QLabel()
@@ -83,10 +86,10 @@ class MainWindow(QMainWindow):
         self.grid_layout.addWidget(self.label_10, 3, 12, 1, 7)
 
         for i in range(5):  # 4행
-            for j in range(7):  # 5열
+            for j in range(20):  # 5열
                 if not ((i == 0 and 0 <= j <= 5) or 
                         (i == 0 and 5 <= j <= 19) or 
-                        (i in [1, 2] and j in [0, 11]) or 
+                        (i in [1, 2] and j in [0, 9]) or 
                         (i == 1 and 12 <= j <= 15) or 
                         (i == 1 and 15 <= j <= 17) or 
                         (i == 1 and 17 <= j <= 19) or 
@@ -137,45 +140,27 @@ class MainWindow(QMainWindow):
         
         
 # ---------------------------------------------------------------------------------------------------------------------        
-        self.label_maker("label_8", "통신이력", 2, 3, 0, 1, 5)
-        self.label_maker("label_9", "A", 2, 3, 6, 1, 5)
-        self.label_maker("label_10", "B", 2, 3, 12, 1, 7)
+        self.label_maker_bold("label_8", "통신이력", 2, 3, 0, 1, 5)
+        self.label_maker_bold("label_9", "A", 2, 3, 6, 1, 5)
+        self.label_maker_bold("label_10", "B", 2, 3, 12, 1, 9)
 
-        self.label_maker("NO_label", "NO", 1, 4, 0)
-        self.label_maker("ALARM_label", "ALARM", 1, 4, 1)
-        self.label_maker("STATE_label", "STATE", 1, 4, 2)
-        self.label_maker("E_CODE_label", "ECODE", 1, 4, 3)
-        self.label_maker("DATETIME_label", "DATETIME", 2, 4, 4)
+        self.label_maker("NO_label", " NO ", 1, 4, 0)
+        self.label_maker("ALARM_label", " ALARM ", 1, 4, 1)
+        self.label_maker("STATE_label", " STATE ", 1, 4, 2)
+        self.label_maker("E_CODE_label", " ECODE ", 1, 4, 3)
+        self.label_maker("DATETIME_label", " DATETIME ", 2, 4, 4)
 
-        self.label_maker("position_label_1", "현재위치", 1, 4, 6)
-        self.label_maker("package_number_label_1", "패키지 번호", 1, 4, 7)
-        self.label_maker("email_label_1", "이메일", 1, 4, 8)
-        self.label_maker("destination_label_1", "목적지", 1, 4, 9)
-        self.label_maker("phone_number_label_1", "차주전화번호", 2, 4, 10)
+        self.label_maker("position_label_1", " 현재위치 ", 1, 4, 6)
+        self.label_maker("package_number_label_1", " 패키지 번호 ", 1, 4, 7)
+        self.label_maker("email_label_1", " 이메일 ", 1, 4, 8)
+        self.label_maker("destination_label_1", " 목적지 ", 1, 4, 9)
+        self.label_maker("phone_number_label_1", " 차주전화번호 ", 2, 4, 10)
 
-        self.label_maker("position_label_2", "현재위치", 1, 4, 12)
-        self.label_maker("package_number_label_2", "패키지 번호", 1, 4, 13)
-        self.label_maker("email_label_2", "이메일", 1, 4, 14, 1, 2)
-        self.label_maker("destination_label_2", "목적지", 1, 4, 16, 1, 2)
-        self.label_maker("phone_number_label_2", "차주전화번호", 2, 4, 18)
-        
-        # self.label_maker("NO_label", "1", 1, 4, 0)
-        # self.label_maker("ALARM_label", "1", 1, 4, 1)
-        # self.label_maker("STATE_label", "1", 1, 4, 2)
-        # self.label_maker("E_CODE_label", "1", 1, 4, 3)
-        # self.label_maker("DATETIME_label", "1", 2, 4, 4)
-
-        # self.label_maker("position_label_1", "1", 1, 4, 6)
-        # self.label_maker("package_number_label_1", "1", 1, 4, 7)
-        # self.label_maker("email_label_1", "1", 1, 4, 8)
-        # self.label_maker("destination_label_1", "1", 1, 4, 9)
-        # self.label_maker("phone_number_label_1", "1", 2, 4, 10)
-
-        # self.label_maker("position_label_2", "1", 1, 4, 12)
-        # self.label_maker("package_number_label_2", "1", 1, 4, 13)
-        # self.label_maker("email_label_2", "1", 1, 4, 14, 1, 2)
-        # self.label_maker("destination_label_2", "1", 1, 4, 16, 1, 2)
-        # self.label_maker("phone_number_label_2", "1", 2, 4, 18)
+        self.label_maker("position_label_2", " 현재위치 ", 1, 4, 12)
+        self.label_maker("package_number_label_2", " 패키지 번호 ", 1, 4, 13, 1, 2)
+        self.label_maker("email_label_2", " 이메일 ", 1, 4, 15, 1, 3)
+        self.label_maker("destination_label_2", " 목적지 ", 1, 4, 18, 1, 2)
+        self.label_maker("phone_number_label_2", " 차주전화번호 ", 2, 4, 20)
 
         self.widget_maker("NO_widget", 3, 5, 0)
         self.widget_maker("ALARM_widget", 3, 5, 1)
@@ -190,10 +175,10 @@ class MainWindow(QMainWindow):
         self.widget_maker("phone_number_widget_1", 4, 5, 10)
 
         self.widget_maker("position_widget_2", 3, 5, 12)
-        self.widget_maker("package_number_widget_2", 3, 5, 13)
-        self.widget_maker("email_widget_2", 3, 5, 14, 1, 2)
-        self.widget_maker("destination_widget_2", 3, 5, 16, 1, 2)
-        self.widget_maker("phone_number_widget_2", 4, 5, 18)
+        self.widget_maker("package_number_widget_2", 3, 5, 13, 1, 2)
+        self.widget_maker("email_widget_2", 3, 5, 15, 1, 3)
+        self.widget_maker("destination_widget_2", 3, 5, 18, 1, 2)
+        self.widget_maker("phone_number_widget_2", 4, 5, 20)
         
         central_widget = QWidget()
         central_widget.setLayout(self.grid_layout)
@@ -202,7 +187,7 @@ class MainWindow(QMainWindow):
     def history_maker(self, label_name, text, style_num, row, col, rowspan=1, colspan=1):
         label = QLabel(text, self)
         setattr(self, label_name, label)
-        label.setFont(font_title)
+        label.setFont(font_bold)
         label.setAlignment(Qt.AlignLeft | Qt.AlignBottom)
         
         if style_num == 2:
@@ -215,6 +200,22 @@ class MainWindow(QMainWindow):
         setattr(self, label_name, label)
         label.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         label.setFont(font_title)
+        label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)
+        
+        if style_num == 1:
+            label.setStyleSheet(border_style_1)#좌상
+        elif style_num == 2:
+            label.setStyleSheet(border_style_2)#좌상우
+        elif style_num == 3:
+            label.setStyleSheet(border_style_3)#좌상하
+        
+        self.grid_layout.addWidget(label, row, col, rowspan, colspan)
+    
+    def label_maker_bold(self, label_name, text, style_num, row, col, rowspan=1, colspan=1):
+        label = QLabel(text, self)
+        setattr(self, label_name, label)
+        label.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        label.setFont(font_bold)
         label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)
         
         if style_num == 1:
--- UI/UI_set_update.py
+++ UI/UI_set_update.py
@@ -18,6 +18,8 @@ def get_option_reset():
     return option_reset
 
 font_title = QFont("NanumSquare", 12)
+font_bold = QFont("NanumSquare", 12)
+font_bold.setBold(True)
 border_style_1 = "border-top: 2px solid black; border-left: 2px solid black;"
 border_style_2 = "border-top: 2px solid black; border-left: 2px solid black; border-right: 2px solid black;"
 border_style_3 = "border-top: 2px solid black; border-left: 2px solid black; border-bottom: 2px solid black;"
@@ -40,7 +42,7 @@ class MainWindow(QMainWindow):
         self.setCentralWidget(central_widget)
 
         self.grid_layout.setSpacing(0)
-        self.grid_layout.setContentsMargins(0, 0, 0, 0)
+        self.grid_layout.setContentsMargins(50, 0, 50, 50)
         
         # 각 열에 대한 비율 설정
         self.grid_layout.setRowStretch(0, 10)
@@ -58,81 +60,73 @@ class MainWindow(QMainWindow):
         self.grid_layout.setColumnStretch(3, 4)
         self.grid_layout.setColumnStretch(4, 36)
         
-        self.grid_layout.setColumnStretch(5, 1)
+        self.grid_layout.setColumnStretch(5, 5)
+        
+        # 각 행에 대한 비율 설정
+        # [통신이력] NO/ALARM/STATE/E_CODE/DATETIME
+        self.grid_layout.setColumnStretch(0, 1)
+        self.grid_layout.setColumnStretch(1, 16)
+        self.grid_layout.setColumnStretch(2, 8)
+        self.grid_layout.setColumnStretch(3, 4)
+        self.grid_layout.setColumnStretch(4, 36)
+        
+        self.grid_layout.setColumnStretch(5, 5)
         
         # [분류] 현재위치/패키지번호/이메일/목적지/차주전화번호
-        self.grid_layout.setColumnStretch(6, 12)
-        self.grid_layout.setColumnStretch(7, 24)
-        self.grid_layout.setColumnStretch(8, 36)
-        self.grid_layout.setColumnStretch(9, 18)
-        self.grid_layout.setColumnStretch(10, 30)
+        self.grid_layout.setColumnStretch(6, 12) # [분류] 현재위치
+        self.grid_layout.setColumnStretch(7, 17) # [분류] 패키지번호
+        self.grid_layout.setColumnStretch(8, 35) # [분류] 이메일
+        self.grid_layout.setColumnStretch(9, 11) # [분류] 목적지
         
-        self.grid_layout.setColumnStretch(11, 1)
+        self.grid_layout.setColumnStretch(10, 30) # Option1 # [분류] 차주전화번호
+        self.grid_layout.setColumnStretch(11, 1) 
+        self.grid_layout.setColumnStretch(12, 12) # [분류] 현재위치
+        self.grid_layout.setColumnStretch(13, 2) # [분류] 패키지번호~
         
-        self.grid_layout.setColumnStretch(12, 8) # Option1
-        self.grid_layout.setColumnStretch(13, 16)
-        self.grid_layout.setColumnStretch(14, 4)
+        self.grid_layout.setColumnStretch(14, 15) # Option2 # [분류] ~패키지번호
+        self.grid_layout.setColumnStretch(15, 4)  # [분류] 이메일~
+        self.grid_layout.setColumnStretch(16, 26) # [분류] ~
         
-        self.grid_layout.setColumnStretch(15, 20) # Option2
-        self.grid_layout.setColumnStretch(16, 3)
+        self.grid_layout.setColumnStretch(17, 5) # Option3 # [분류] ~이메일
+        self.grid_layout.setColumnStretch(18, 2) # [분류] 목적지~
+        self.grid_layout.setColumnStretch(19, 9) # [분류] ~목적지
+        self.grid_layout.setColumnStretch(20, 30) # [분류] 차주전화번호
         
-        self.grid_layout.setColumnStretch(17, 7) # Option3
-        self.grid_layout.setColumnStretch(18, 30)
-        
-        label_1 = QLabel(self)
-        label_1.setMinimumSize(0, 0)
+        label_1 = QLabel()
         self.grid_layout.addWidget(label_1, 0, 0, 1, 5)
-
-        label_2 = QLabel(self)
-        label_2.setMinimumSize(0, 0)
+        label_2 = QLabel()
         self.grid_layout.addWidget(label_2, 0, 5, 1, 14)
-
-        self.label_3 = QLabel(self)
-        self.label_3.setMinimumSize(0, 0)
-        self.grid_layout.addWidget(self.label_3, 1, 0, 2, 11)
-
-        label_4 = QLabel(self)
-        label_4.setMinimumSize(0, 0)
+        self.label_3 = QLabel()
+        self.grid_layout.addWidget(self.label_3, 1, 0, 2, 9)
+        label_4 = QLabel()
         self.grid_layout.addWidget(label_4, 1, 12, 1, 3)
-
-        label_5 = QLabel(self)
-        label_5.setMinimumSize(0, 0)
+        label_5 = QLabel()
         self.grid_layout.addWidget(label_5, 1, 15, 1, 2)
-
-        label_6 = QLabel(self)
-        label_6.setMinimumSize(0, 0)
+        label_6 = QLabel()
         self.grid_layout.addWidget(label_6, 1, 17, 1, 2)
-
-        label_7 = QLabel(self)
-        label_7.setMinimumSize(0, 0)
+        label_7 = QLabel()
         self.grid_layout.addWidget(label_7, 2, 12, 1, 7)
-
-        label_8 = QLabel(self)
-        label_8.setMinimumSize(0, 0)
+        label_8 = QLabel()
         self.grid_layout.addWidget(label_8, 3, 0, 1, 5)
-
-        self.label_9 = QLabel(self)
-        self.label_9.setMinimumSize(0, 0)
+        self.label_9 = QLabel()
         self.grid_layout.addWidget(self.label_9, 3, 6, 1, 5)
-
-        self.label_10 = QLabel(self)
-        self.label_10.setMinimumSize(0, 0)
+        self.label_10 = QLabel()
         self.grid_layout.addWidget(self.label_10, 3, 12, 1, 7)
 
-        # for i in range(5):  # 4행
-        #     for j in range(7):  # 5열
-        #         if not ((i == 0 and 0 <= j <= 5) or 
-        #                 (i == 0 and 5 <= j <= 19) or 
-        #                 (i in [1, 2] and j in [0, 11]) or 
-        #                 (i == 1 and 12 <= j <= 15) or 
-        #                 (i == 1 and 15 <= j <= 17) or 
-        #                 (i == 1 and 17 <= j <= 19) or 
-        #                 (i == 2 and 12 <= j <= 19) or 
-        #                 (i == 3 and 0 <= j <= 5) or 
-        #                 (i == 3 and 6 <= j <= 11) or 
-        #                 (i == 3 and 12 <= j <= 19)):
-        #             label = QLabel()
-        #             self.grid_layout.addWidget(label, i, j)
+        for i in range(5):  # 4행
+            for j in range(20):  # 5열
+                if not ((i == 0 and 0 <= j <= 5) or 
+                        (i == 0 and 5 <= j <= 19) or 
+                        (i in [1, 2] and j in [0, 9]) or 
+                        (i == 1 and 12 <= j <= 15) or 
+                        (i == 1 and 15 <= j <= 17) or 
+                        (i == 1 and 17 <= j <= 19) or 
+                        (i == 2 and 12 <= j <= 19) or 
+                        (i == 3 and 0 <= j <= 5) or 
+                        (i == 3 and 6 <= j <= 11) or 
+                        (i == 3 and 12 <= j <= 19)):
+                    label = QLabel()
+                    self.grid_layout.addWidget(label, i, j)
         
 # ---------------------------------------------------------------------------------------------------------------------
         # 윈도우 설정
@@ -175,12 +169,10 @@ class MainWindow(QMainWindow):
         
 # ---------------------------------------------------------------------------------------------------------------------
         # 옵션 버튼 설정
-        self.option_buttons = [
-            OptionButton('domfor_ON.png', 'domfor_OFF.png', 'Option1', self),
-            OptionButton('fragile_ON.png', 'fragile_OFF.png', 'Option2', self),
-            OptionButton('courier_ON.png', 'courier_OFF.png', 'Option3', self),
-        ]
-        button_positions = [(1, 12, 1, 3), (1, 15, 1, 2), (1, 17, 1, 2)]
+        self.option_buttons = [OptionButton('opt1_ON.png', 'opt1_OFF.png', 'Option1', self),
+                               OptionButton('opt2_ON.png', 'opt2_OFF.png', 'Option2', self),
+                               OptionButton('opt3_ON.png', 'opt3_OFF.png', 'Option3', self)]
+        button_positions = [(1, 10, 1, 4), (1, 14, 1, 3), (1, 17, 1, 4)]
         for i, option_button in enumerate(self.option_buttons):
             transparent_button = TransparentButton(option_button)
             transparent_button.setFixedSize(240, 135)
@@ -191,51 +183,33 @@ class MainWindow(QMainWindow):
         # 작업 중지 버튼 설정
         self.pause_button_label = QLabel(self)
         pause_button_pixmap = QPixmap('pause_button.png')
-        pause_button_pixmap = pause_button_pixmap.scaled(650, 200, Qt.KeepAspectRatio, Qt.SmoothTransformation)
+        pause_button_pixmap = pause_button_pixmap.scaled(850, 200, Qt.KeepAspectRatio, Qt.SmoothTransformation)
         self.pause_button_label.setPixmap(pause_button_pixmap)
         self.pause_button_label.setAlignment(Qt.AlignHCenter | Qt.AlignVCenter)
         self.pause_button_label.mousePressEvent = self.pauseClicked
-        self.grid_layout.addWidget(self.pause_button_label, 2, 12, 1, 7)
+        self.grid_layout.addWidget(self.pause_button_label, 2, 10, 1, 11)
         
-        self.label_maker("label_8", "통신이력", 2, 3, 0, 1, 5)
-        self.label_maker("label_9", "A", 2, 3, 6, 1, 5)
-        self.label_maker("label_10", "B", 2, 3, 12, 1, 7)
+        self.label_maker_bold("label_8", "통신이력", 2, 3, 0, 1, 5)
+        self.label_maker_bold("label_9", "A", 2, 3, 6, 1, 5)
+        self.label_maker_bold("label_10", "B", 2, 3, 12, 1, 9)
 
-        # self.label_maker("NO_label", "NO", 1, 4, 0)
-        # self.label_maker("ALARM_label", "ALARM", 1, 4, 1)
-        # self.label_maker("STATE_label", "STATE", 1, 4, 2)
-        # self.label_maker("E_CODE_label", "ECODE", 1, 4, 3)
-        # self.label_maker("DATETIME_label", "DATETIME", 2, 4, 4)
+        self.label_maker("NO_label", " NO ", 1, 4, 0)
+        self.label_maker("ALARM_label", " ALARM ", 1, 4, 1)
+        self.label_maker("STATE_label", " STATE ", 1, 4, 2)
+        self.label_maker("E_CODE_label", " ECODE ", 1, 4, 3)
+        self.label_maker("DATETIME_label", " DATETIME ", 2, 4, 4)
 
-        # self.label_maker("position_label_1", "현재위치", 1, 4, 6)
-        # self.label_maker("package_number_label_1", "패키지 번호", 1, 4, 7)
-        # self.label_maker("email_label_1", "이메일", 1, 4, 8)
-        # self.label_maker("destination_label_1", "목적지", 1, 4, 9)
-        # self.label_maker("phone_number_label_1", "차주전화번호", 2, 4, 10)
+        self.label_maker("position_label_1", " 현재위치 ", 1, 4, 6)
+        self.label_maker("package_number_label_1", " 패키지 번호 ", 1, 4, 7)
+        self.label_maker("email_label_1", " 이메일 ", 1, 4, 8)
+        self.label_maker("destination_label_1", " 목적지 ", 1, 4, 9)
+        self.label_maker("phone_number_label_1", " 차주전화번호 ", 2, 4, 10)
 
-        # self.label_maker("position_label_2", "현재위치", 1, 4, 12)
-        # self.label_maker("package_number_label_2", "패키지 번호", 1, 4, 13)
-        # self.label_maker("email_label_2", "이메일", 1, 4, 14, 1, 2)
-        # self.label_maker("destination_label_2", "목적지", 1, 4, 16, 1, 2)
-        # self.label_maker("phone_number_label_2", "차주전화번호", 2, 4, 18)
-        
-        self.label_maker("NO_label", "1", 1, 4, 0)
-        self.label_maker("ALARM_label", "1", 1, 4, 1)
-        self.label_maker("STATE_label", "1", 1, 4, 2)
-        self.label_maker("E_CODE_label", "1", 1, 4, 3)
-        self.label_maker("DATETIME_label", "1", 2, 4, 4)
-
-        self.label_maker("position_label_1", "1", 1, 4, 6)
-        self.label_maker("package_number_label_1", "1", 1, 4, 7)
-        self.label_maker("email_label_1", "1", 1, 4, 8)
-        self.label_maker("destination_label_1", "1", 1, 4, 9)
-        self.label_maker("phone_number_label_1", "1", 2, 4, 10)
-
-        self.label_maker("position_label_2", "1", 1, 4, 12)
-        self.label_maker("package_number_label_2", "1", 1, 4, 13)
-        self.label_maker("email_label_2", "1", 1, 4, 14, 1, 2)
-        self.label_maker("destination_label_2", "1", 1, 4, 16, 1, 2)
-        self.label_maker("phone_number_label_2", "1", 2, 4, 18)
+        self.label_maker("position_label_2", " 현재위치 ", 1, 4, 12)
+        self.label_maker("package_number_label_2", " 패키지 번호 ", 1, 4, 13, 1, 2)
+        self.label_maker("email_label_2", " 이메일 ", 1, 4, 15, 1, 3)
+        self.label_maker("destination_label_2", " 목적지 ", 1, 4, 18, 1, 2)
+        self.label_maker("phone_number_label_2", " 차주전화번호 ", 2, 4, 20)
 
         self.widget_maker("NO_widget", 3, 5, 0)
         self.widget_maker("ALARM_widget", 3, 5, 1)
@@ -250,10 +224,10 @@ class MainWindow(QMainWindow):
         self.widget_maker("phone_number_widget_1", 4, 5, 10)
 
         self.widget_maker("position_widget_2", 3, 5, 12)
-        self.widget_maker("package_number_widget_2", 3, 5, 13)
-        self.widget_maker("email_widget_2", 3, 5, 14, 1, 2)
-        self.widget_maker("destination_widget_2", 3, 5, 16, 1, 2)
-        self.widget_maker("phone_number_widget_2", 4, 5, 18)
+        self.widget_maker("package_number_widget_2", 3, 5, 13, 1, 2)
+        self.widget_maker("email_widget_2", 3, 5, 15, 1, 3)
+        self.widget_maker("destination_widget_2", 3, 5, 18, 1, 2)
+        self.widget_maker("phone_number_widget_2", 4, 5, 20)
         
         central_widget = QWidget()
         central_widget.setLayout(self.grid_layout)
@@ -273,7 +247,24 @@ class MainWindow(QMainWindow):
     def label_maker(self, label_name, text, style_num, row, col, rowspan=1, colspan=1):
         label = QLabel(text, self)
         setattr(self, label_name, label)
+        label.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         label.setFont(font_title)
+        label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)
+        
+        if style_num == 1:
+            label.setStyleSheet(border_style_1)#좌상
+        elif style_num == 2:
+            label.setStyleSheet(border_style_2)#좌상우
+        elif style_num == 3:
+            label.setStyleSheet(border_style_3)#좌상하
+        
+        self.grid_layout.addWidget(label, row, col, rowspan, colspan)
+    
+    def label_maker_bold(self, label_name, text, style_num, row, col, rowspan=1, colspan=1):
+        label = QLabel(text, self)
+        setattr(self, label_name, label)
+        label.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
+        label.setFont(font_bold)
         label.setAlignment(Qt.AlignHCenter | Qt.AlignBottom)
         
         if style_num == 1:
@@ -288,6 +279,8 @@ class MainWindow(QMainWindow):
     def widget_maker(self, widget_name, style_num, row, col, rowspan=1, colspan=1):
         widget = QListWidget(self)
         setattr(self, widget_name, widget)
+        widget.setMinimumSize(1, 1)
+        widget.setSizePolicy(QSizePolicy.Minimum, QSizePolicy.Minimum)
         
         if style_num == 1:
             widget.setStyleSheet(border_style_1)#좌상
@@ -390,8 +383,8 @@ class OptionButton(QWidget):
             return
         
         self.label = QLabel(self)
-        self.setFixedSize(240, 270)  # 초기 크기 설정
-        self.label.setFixedSize(240, 270)
+        self.setFixedSize(300, 300)  # 초기 크기 설정
+        self.label.setFixedSize(280, 280)
         self.setScaledPixmap()
         
         self.transparent_button = TransparentButton(self)
@@ -406,16 +399,10 @@ class OptionButton(QWidget):
         self.transparent_button.raise_()
 
     def setScaledPixmap(self):
-        # if self.is_on:
-        #     pixmap = self.on_pixmap.scaled(self.label.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
-        # else:
-        #     pixmap = self.off_pixmap.scaled(self.label.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
-        # self.label.setPixmap(pixmap)
-        
         if self.is_on:
-            pixmap = self.on_pixmap.scaled(self.label.size())
+            pixmap = self.on_pixmap.scaled(self.label.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
         else:
-            pixmap = self.off_pixmap.scaled(self.label.size())
+            pixmap = self.off_pixmap.scaled(self.label.size(), Qt.KeepAspectRatioByExpanding, Qt.SmoothTransformation)
         self.label.setPixmap(pixmap)
         # self.update()
     
